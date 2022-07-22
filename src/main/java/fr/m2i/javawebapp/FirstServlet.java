5f0906609795eda699489f1c746346aa0adfcbdf
KylianCrv <kylian.crv@hotmail.com>
2022-07-22 14:40:58 +0200

    Correction exo1

--- src/main/java/fr/m2i/javawebapp/FirstServlet.java
+++ src/main/java/fr/m2i/javawebapp/FirstServlet.java
@@ -1,23 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package fr.m2i.javawebapp;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author ishish
- */
 public class FirstServlet extends HttpServlet {
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -30,7 +20,8 @@ public class FirstServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 //        super.doGet(request, response);
-        response.sendRedirect("firstServlet.html");
+        response.sendRedirect("SecondServlet");
+//        this.getServletContext().getRequestDispatcher("/firstServlet.html").forward(request, response);
     }
 
     /**
@@ -55,6 +46,6 @@ public class FirstServlet extends HttpServlet {
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    }
 
 }
--- src/main/java/fr/m2i/javawebapp/SecondServlet.java
+++ src/main/java/fr/m2i/javawebapp/SecondServlet.java
@@ -1,23 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package fr.m2i.javawebapp;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author ishish
- */
 public class SecondServlet extends HttpServlet {
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -29,7 +19,9 @@ public class SecondServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.sendRedirect("secondServlet.html");
+//        response.sendRedirect("secondServlet.html");
+//        this.getServletContext().getRequestDispatcher("/secondServlet.html").forward(request, response);
+        response.sendRedirect("index.html");
 
     }
 
